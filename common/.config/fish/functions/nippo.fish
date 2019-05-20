bfede72db755f966d0b161be8666cea69110cb49
kenta kuroda <kenta-kuroda@cookpad.com>
2019-05-21 00:18:39 +0900

    add push option

--- common/.config/fish/functions/nippo.fish
+++ common/.config/fish/functions/nippo.fish
@@ -1,10 +1,26 @@
 function nippo
+  argparse -n nippo 'p/push' -- $argv
+  or return 1
+
   set zatsuPath $WORKSPACE/zatsu
   set templatePath $zatsuPath/nippo_template.md
   set nippoDir $zatsuPath/nippo/
   set day (date "+%Y-%m-%d")
   set filename $day.md
   set filePath $nippoDir$filename
+
+  if set -lq _flag_push
+    set currentPath (pwd)
+    cd $zatsuPath
+    echo (pwd)
+
+    git add $nippoDir
+    git commit -m "nippo/$filename"
+    gpush
+
+    cd $currentPath
+    return
+  end
 
   if [ -f $filePath ]
     echo "today's nippo exists"
