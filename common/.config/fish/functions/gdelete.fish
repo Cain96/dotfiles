51de390c07176d2e300e3043af06ff0d5b9992c4
kenta kuroda <kenta-kuroda@cookpad.com>
2019-07-05 19:57:22 +0900

    add delete remote branches command

--- common/.config/fish/functions/gdelete.fish
+++ common/.config/fish/functions/gdelete.fish
@@ -1,11 +1,20 @@
 function gdelete
-  set deleteBranches (git branch --merged | grep -vE '^\*|develop$|master$' | sed "s/  *//g" )
+  set deleteBranches (git branch --merged | grep -vE '^\*|develop$|master$' | sed "s/  *//g")
+  set deleteRemoteBranches (git branch -r --merged | grep -vE '^\*|develop$|master$' | sed "s/  *//g")
   set deleteBranchesCount (count $deleteBranches)
-  
+  set deleteRemoteBranchesCount (count $deleteRemoteBranches)
+
   if [ $deleteBranchesCount -gt 0 ]
     echo git branch -d $deleteBranches
     git branch -d $deleteBranches
-  else
+  end
+
+  if [ $deleteRemoteBranchesCount -gt 0 ]
+    echo git branch -r -d $deleteRemoteBranches
+    git branch -r -d $deleteRemoteBranches
+  end
+
+  if [ $deleteBranchesCount -eq 0 ] && [ $deleteRemoteBranchesCount -eq 0 ]
     echo "merged branch does not exist"
   end
 end
